5019d7f9b64c6261658f03b6a615d308b5129859
genjose12345 <85368999+genjose12345@users.noreply.github.com>
2023-04-09 17:51:38 -0400

    Add files via upload

---
+++ round 1/RoundOneController.java
@@ -0,0 +1,93 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import java.util.*;
+import java.io.*;
+import javafx.scene.control.Button;
+import java.util.ArrayList;
+import java.nio.file.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+///////////////////////////
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+
+public class RoundOneController {
+
+    @FXML
+    private Label team2;
+
+    @FXML
+    private Label team3;
+
+    @FXML
+    private Label team4;
+
+    @FXML
+    private Label team5;
+
+    @FXML
+    private Label team6;
+
+    @FXML
+    private Label team7;
+
+    @FXML
+    private Label team8;
+
+    @FXML
+    private Label team10;
+
+    @FXML
+    private Label team9;
+
+    @FXML
+    private Label team1;
+
+    @FXML
+    private Label tbd1;
+
+    @FXML
+    private Label tbd2;
+
+    @FXML
+    private Label tbd3;
+
+    @FXML
+    private Label tbd4;
+
+    @FXML
+    private Label tbd7;
+
+    @FXML
+    private Label tbd6;
+
+    @FXML
+    private Label tbd5;
+
+    @FXML
+    private Label tbd8;
+
+    @FXML
+    private Label tbd9;
+
+}
